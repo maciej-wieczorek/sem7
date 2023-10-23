14a6fffff4bddbb544bcbe50f338842948674c9d
Maciej Wieczorek <maciejmail.wieczorek@gmail.com>
2023-10-23 19:26:48 +0200

    bd project

---
+++ BigData/projekt/p1-MapReduce/src/main/java/com/example/bigdata/CountStartingZonePassengers.java
@@ -0,0 +1,86 @@
+package com.example.bigdata;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+public class CountStartingZonePassengers extends Configured implements Tool {
+
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new CountStartingZonePassengers(), args);
+        System.exit(res);
+    }
+
+    public int run(String[] args) throws Exception {
+        Job job = Job.getInstance(getConf(), "CountStartingZonePassengers");
+        job.setJarByClass(this.getClass());
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(MyMapper.class);
+        job.setCombinerClass(MyCombiner.class);
+        job.setReducerClass(MyReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        public void map(LongWritable offset, Text lineText, Context context) {
+            try {
+                if (offset.get() != 0) {
+                    String line = lineText.toString();
+                    String[] split = line.split(",");
+                    String month = split[1];
+                    String zone = split[7];
+                    Text key = new Text();
+                    key.set(month+zone);
+                    context.write(key, new IntWritable(1));
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context) throws IOException, InterruptedException {
+
+            int count = 0;
+            for (IntWritable val : values) {
+                count += val.get();
+            }
+
+            context.write(key, new IntWritable(count));
+
+        }
+    }
+
+    public static class MyCombiner extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int count = 0;
+            for (IntWritable val : values) {
+                count += val.get();
+            }
+            context.write(key, new IntWritable(count));
+        }
+    }
+}
